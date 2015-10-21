corrected last kernel, added r2 score computation

DEI/GP_model.py
@@ -7,7 +7,7 @@ Date: 21 Oct 2015
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from kernels import gaussian_kernel, gaussian_kernel_2
+from kernels import gaussian_kernel, gaussian_kernel_2, locally_periodic_kernel
 sns.set(color_codes=True)
 
 
@@ -27,6 +27,12 @@ def predict(X=None,
         
     elif use_kernel=="gaussian_2":
         kernel = gaussian_kernel_2
+        
+    elif use_kernel == "locally_periodic":
+        kernel = locally_periodic_kernel
+        
+    else:
+        raise ValueError("%s kernel not implemented:" % use_kernel)
     
     K = kernel(X1=X[None, :], 
                X2=X[:, None],
@@ -54,21 +60,31 @@ def predict(X=None,
         return yy_mean, yy_var
 
     if not hasattr(xstar, "__len__"):
-        y_mean, y_var = get_y(xstar)
+        y_predicted, y_var = get_y(xstar)
 
     else:
-        y_mean = []
+        y_predicted = []
         y_var = []
         for xxstar in xstar:
             
             yy_mean, yy_var = get_y(xxstar)
-            y_mean.append(yy_mean)
+            y_predicted.append(yy_mean)
             y_var.append(yy_var)
             
     print("done")
     print("-"*50)
     
+    print('computing score...')
+    
+    ssres = np.sum(np.power(y_predicted - truth, 2))
+    sstot = np.sum(np.power(y_predicted - np.mean(y_predicted), 2))
+    r2 = 1 - ssres / sstot
+    
+    print("done")
+    print("-"*50)
+    
     print("displaying results...")
+    plt.suptitle("R^2 : " + str(np.round(r2, 4)))
     
     plt.plot(X,
              Y,
@@ -81,7 +97,7 @@ def predict(X=None,
              alpha=0.5)
     
     plt.errorbar(xstar,
-                 y_mean,
+                 y_predicted,
                  c='red',
                  yerr=1.96 * np.sqrt(y_var),
                  alpha=0.5)
@@ -89,4 +105,4 @@ def predict(X=None,
     
     print("done")
 
-    return y_mean, y_var
+    return y_predicted, y_var

DEI/kernels.py
@@ -51,10 +51,10 @@ def locally_periodic_kernel(X1=None,
                             params=None,
                             **kwargs):
     
-    sigma, sigma_n, p, scale, scale_2 = params
+    sigma_n, sigma_f, scale, p, scale_2 = params
     
     D = X1 - X2
-    K = sigma ** 2 * np.exp(-np.power(D, 2) / (2 * scale ** 2))
+    K = sigma_f ** 2 * np.exp(-np.power(D, 2) / (2 * scale ** 2))
     K *= np.exp(-2 * np.sin(np.pi * np.abs(D) / p) ** 2 / (scale_2 ** 2))
     
     if len(D.shape) == 2:

DEI/tune.py
@@ -7,7 +7,7 @@ Date: 21 Oct 2015
 import numpy as np
 import scipy.stats
 import scipy.optimize
-from kernels import gaussian_kernel, gaussian_kernel_2
+from kernels import gaussian_kernel, gaussian_kernel_2, locally_periodic_kernel
 
 def optimize_hyperparameters(X=None,
                              Y=None,
@@ -15,20 +15,26 @@ def optimize_hyperparameters(X=None,
     
     print("optimizing hyper-parameters...")
     
-    if use_kernel =="gaussian":
+    if use_kernel == "gaussian":
         kernel = gaussian_kernel
         mean_params = np.array([1., 1., 10.])
         sigma_params = np.array([10., 10., 10.])
         init_theta = np.array([10., 0.5, 25])
         
-    elif use_kernel=="gaussian_2":
+    elif use_kernel == "gaussian_2":
         kernel = gaussian_kernel_2
         mean_params = np.array([1., 1., 10., 0.1, 248.])
         sigma_params = np.array([10., 10., 10., 10., 10.])
         init_theta = np.array([10., 0.5, 25, 1., 250.])
         
+    elif use_kernel == "locally_periodic":
+        kernel = locally_periodic_kernel
+        mean_params = np.array([1., 1., 10., 2, 120])
+        sigma_params = np.array([10., 10., 10., 10., 10.])
+        init_theta = np.array([10., 0.5, 25, 2., 100.])
+        
     else:
-        raise ValueError("%s kernel not implemented: should be 'gaussian' or 'gaussian_2'" % use_kernel)
+        raise ValueError("%s kernel not implemented:" % use_kernel)
         
     bounds = [(1e-2, None)] * len(init_theta)
     
@@ -84,8 +90,11 @@ def optimize_hyperparameters(X=None,
     print('sigma_f :', params_found[0])
     print('sigma_n :', params_found[1])
     print('scale :', params_found[2])
-    if use_kernel=="gaussian_2":
+    if use_kernel == "gaussian_2":
         print('sigma_f_2 :', params_found[3])
+        print('scale_2 :', params_found[4])
+    elif use_kernel == "locally_periodic":
+        print('p :', params_found[3])
         print('scale_2 :', params_found[4])
     print('-' * 50)
     